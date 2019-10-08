d83af88671badfdd7f04b679fd31fde223bc4170
Tobbot Zenz <tzenz@ga-u6.21-school.ru>
2019-10-08 14:37:18 +0300

    hi

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_nex_line.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tzenz <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/24 11:50:26 by tzenz             #+#    #+#             */
+/*   Updated: 2019/10/08 12:51:44 by tzenz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 5
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+# include "libft/libft.h"
+# include <stdio.h>
+
+int		get_next_line(const int fd, char **line);
+#endif
